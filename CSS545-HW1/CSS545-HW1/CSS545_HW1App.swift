b0fbc0a121bae7db281382402a8f467dab8e0fa4
kr1s0404 <tylerlolz0404@gmail.com>
2024-10-03 20:25:29 -0700

    Create Xcode project for HW1

---
+++ CSS545-HW1/CSS545-HW1/CSS545_HW1App.swift
@@ -0,0 +1,17 @@
+//
+//  CSS545_HW1App.swift
+//  CSS545-HW1
+//
+//  Created by Kris on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct CSS545_HW1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSS545-HW1/CSS545-HW1/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CSS545-HW1
+//
+//  Created by Kris on 10/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
