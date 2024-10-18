75a3b98eed029a9bff1c7844c09f9448e290ee97
kr1s0404 <tylerlolz0404@gmail.com>
2024-10-18 03:08:40 -0700

    Add caption text box view

--- CSS545-HW2/CSS545-HW2/CaptionEditorView.swift
+++ CSS545-HW2/CSS545-HW2/CaptionEditorView.swift
@@ -7,12 +7,45 @@
 
 import SwiftUI
 
-struct CaptionEditorView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+struct CaptionEditorView: View
+{
+    @Binding var isPresented: Bool
+    @State private var captionText: String = ""
+    var onSave: (String) -> Void
+    
+    var body: some View 
+    {
+        NavigationStack
+        {
+            VStack {
+                TextField("Enter Caption", text: $captionText)
+                    .textFieldStyle(RoundedBorderTextFieldStyle())
+                    .padding()
+                
+                Spacer()
+            }
+            .navigationTitle("Add Caption")
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        isPresented = false
+                        captionText = ""
+                    }
+                }
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    Button("Save") {
+                        onSave(captionText)
+                        isPresented = false
+                        captionText = ""
+                    }
+                    .disabled(captionText.isEmpty)
+                }
+            }
+        }
     }
 }
 
+
 #Preview {
-    CaptionEditorView()
+    CaptionEditorView(isPresented: .constant(true), onSave: { string in  })
 }
