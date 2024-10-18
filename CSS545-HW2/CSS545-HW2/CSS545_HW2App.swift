5f5ba03ff2b9605ca565c22fe41022e359b44507
kr1s0404 <tylerlolz0404@gmail.com>
2024-10-18 03:25:36 -0700

    Update entry point for SwiftData

--- CSS545-HW2/CSS545-HW2/CSS545_HW2App.swift
+++ CSS545-HW2/CSS545-HW2/CSS545_HW2App.swift
@@ -6,12 +6,17 @@
 //
 
 import SwiftUI
+import SwiftData
 
 @main
-struct CSS545_HW2App: App {
-    var body: some Scene {
-        WindowGroup {
+struct CSS545_HW2App: App 
+{
+    var body: some Scene 
+    {
+        WindowGroup 
+        {
             ContentView()
         }
+        .modelContainer(for: PhotoItem.self)
     }
 }
