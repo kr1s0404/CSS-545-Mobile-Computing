c06412fe079babebf59f6d18ec92430b0c3979b8
kr1s0404 <tylerlolz0404@gmail.com>
2024-10-17 18:50:05 -0700

    Create Xcode project for HW2

---
+++ CSS545-HW2/CSS545-HW2/CSS545_HW2App.swift
@@ -0,0 +1,17 @@
+//
+//  CSS545_HW2App.swift
+//  CSS545-HW2
+//
+//  Created by Kris on 10/17/24.
+//
+
+import SwiftUI
+
+@main
+struct CSS545_HW2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSS545-HW2/CSS545-HW2/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  CSS545-HW2
+//
+//  Created by Kris on 10/17/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
